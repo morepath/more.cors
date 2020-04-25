1247120ac9495a7694b65fbbd4ce8b64577ca2dc
Henri Hulski <henri.hulski@gazeta.pl>
2020-04-25 21:33:19 +0200

    Format code base with Black

--- more/cors/action.py
+++ more/cors/action.py
@@ -3,17 +3,19 @@ import dectate
 
 class CORSAction(dectate.Action):
 
-    filter_convert = {
-        'model': dectate.convert_dotted_name
-    }
+    filter_convert = {"model": dectate.convert_dotted_name}
     app_class_arg = True
 
-    def __init__(self, model, view_name=None,
-                 allowed_origin=None,
-                 allowed_headers=None,
-                 expose_headers=None,
-                 allow_credentials=None,
-                 max_age=None):
+    def __init__(
+        self,
+        model,
+        view_name=None,
+        allowed_origin=None,
+        allowed_headers=None,
+        expose_headers=None,
+        allow_credentials=None,
+        max_age=None,
+    ):
         self.model = model
         self.view_name = view_name
         self.allowed_origin = allowed_origin
@@ -30,21 +32,26 @@ class CORSAction(dectate.Action):
         if allowed_origin is not None:
             app_class.get_cors_allowed_origin.register(
                 lambda self, model, request, requested_origin: allowed_origin,
-                model=self.model, view_name=self.view_name)
+                model=self.model,
+                view_name=self.view_name,
+            )
 
         allowed_headers = self.allowed_headers
         if allowed_headers is not None:
             app_class.get_cors_allowed_headers.register(
-                (lambda self, model, request,
-                 requested_headers: allowed_headers),
-                model=self.model, view_name=self.view_name
+                (
+                    lambda self, model, request, requested_headers: allowed_headers
+                ),
+                model=self.model,
+                view_name=self.view_name,
             )
 
         expose_headers = self.expose_headers
         if expose_headers is not None:
             app_class.get_cors_expose_headers.register(
                 lambda self, model, request: expose_headers,
-                model=self.model, view_name=self.view_name
+                model=self.model,
+                view_name=self.view_name,
             )
 
         allow_credentials = self.allow_credentials
@@ -52,7 +59,8 @@ class CORSAction(dectate.Action):
         if allow_credentials is not None:
             app_class.get_cors_allow_credentials.register(
                 lambda self, model, request: allow_credentials,
-                model=self.model, view_name=self.view_name
+                model=self.model,
+                view_name=self.view_name,
             )
 
         max_age = self.max_age
@@ -60,5 +68,6 @@ class CORSAction(dectate.Action):
         if max_age is not None:
             app_class.get_cors_max_age.register(
                 lambda self, model, request: max_age,
-                model=self.model, view_name=self.view_name
+                model=self.model,
+                view_name=self.view_name,
             )
--- more/cors/main.py
+++ more/cors/main.py
@@ -15,56 +15,68 @@ class App(morepath.App):
         cls._cors(*args, **kwargs)(None)
 
     @reg.dispatch_method(
-        reg.match_instance('model'),
-        reg.match_key('view_name',
-                      lambda self, model, request,
-                      requested_origin: request.view_name))
+        reg.match_instance("model"),
+        reg.match_key(
+            "view_name",
+            lambda self, model, request, requested_origin: request.view_name,
+        ),
+    )
     def get_cors_allowed_origin(self, model, request, requested_origin):
         return self.settings.cors.allowed_origin
 
     @reg.dispatch_method(
-        reg.match_instance('model'),
-        reg.match_key('view_name',
-                      lambda self, model, request,
-                      requested_method: request.view_name))
+        reg.match_instance("model"),
+        reg.match_key(
+            "view_name",
+            lambda self, model, request, requested_method: request.view_name,
+        ),
+    )
     def get_cors_allowed_methods(self, model, request, requested_method):
         if model is None:
             return self.settings.cors.allowed_verbs
         res = []
         for m in self.settings.cors.allowed_verbs:
             f = self.get_view.by_predicates(
-                model=model.__class__, name=request.view_name,
-                request_method=m)
+                model=model.__class__, name=request.view_name, request_method=m
+            )
             if f and f.component:
                 res.append(m)
         return res
 
     @reg.dispatch_method(
-        reg.match_instance('model'),
-        reg.match_key('view_name',
-                      lambda self, model, request,
-                      requested_headers: request.view_name))
+        reg.match_instance("model"),
+        reg.match_key(
+            "view_name",
+            lambda self, model, request, requested_headers: request.view_name,
+        ),
+    )
     def get_cors_allowed_headers(self, model, request, requested_headers):
         return self.settings.cors.allowed_headers
 
     @reg.dispatch_method(
-        reg.match_instance('model'),
-        reg.match_key('view_name',
-                      lambda self, model, request: request.view_name))
+        reg.match_instance("model"),
+        reg.match_key(
+            "view_name", lambda self, model, request: request.view_name
+        ),
+    )
     def get_cors_expose_headers(self, model, request):
         return self.settings.cors.expose_headers
 
     @reg.dispatch_method(
-        reg.match_instance('model'),
-        reg.match_key('view_name',
-                      lambda self, model, request: request.view_name))
+        reg.match_instance("model"),
+        reg.match_key(
+            "view_name", lambda self, model, request: request.view_name
+        ),
+    )
     def get_cors_allow_credentials(self, model, request):
         return self.settings.cors.allow_credentials
 
     @reg.dispatch_method(
-        reg.match_instance('model'),
-        reg.match_key('view_name',
-                      lambda self, model, request: request.view_name))
+        reg.match_instance("model"),
+        reg.match_key(
+            "view_name", lambda self, model, request: request.view_name
+        ),
+    )
     def get_cors_max_age(self, model, request):
         return self.settings.cors.max_age
 
@@ -72,22 +84,22 @@ class App(morepath.App):
 CORSApp = App
 
 
-@App.setting_section(section='cors')
+@App.setting_section(section="cors")
 def cors_settings():
     return {
-        'allowed_verbs': ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-        'allowed_origin': '*',
-        'expose_headers': ['Content-Type', 'Authorization'],
-        'allowed_headers': ['Content-Type', 'Authorization'],
-        'max_age': 60,
-        'allow_credentials': False
+        "allowed_verbs": ["GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"],
+        "allowed_origin": "*",
+        "expose_headers": ["Content-Type", "Authorization"],
+        "allowed_headers": ["Content-Type", "Authorization"],
+        "max_age": 60,
+        "allow_credentials": False,
     }
 
 
 @App.tween_factory(over=morepath.EXCVIEW)
 def cors_tween(app, handler):
     def cors_handler(request):
-        preflight = request.method == 'OPTIONS'
+        preflight = request.method == "OPTIONS"
         if preflight:
             response = request.ResponseClass()
         else:
@@ -101,54 +113,62 @@ def cors_tween(app, handler):
             context = app
 
         _marker = []
-        if getattr(request, 'view_name', _marker) is _marker:
+        if getattr(request, "view_name", _marker) is _marker:
             request.view_name = get_view_name(request.unconsumed)
 
         # Access-Control-Allow-Methods
-        requested_method = request.headers.get('Access-Control-Request-Method')
+        requested_method = request.headers.get("Access-Control-Request-Method")
 
         if preflight:
             allowed_methods = app.get_cors_allowed_methods(
-                context, request, requested_method)
+                context, request, requested_method
+            )
             if not allowed_methods:
                 return request.ResponseClass(status_code=404)
-            response.headers.add('Access-Control-Allow-Methods',
-                                 ','.join(['OPTIONS'] + allowed_methods))
+            response.headers.add(
+                "Access-Control-Allow-Methods",
+                ",".join(["OPTIONS"] + allowed_methods),
+            )
 
         # Access-Control-Allow-Headers
         requested_headers = request.headers.get(
-            'Access-Control-Request-Headers')
+            "Access-Control-Request-Headers"
+        )
 
         if preflight:
             allowed_headers = app.get_cors_allowed_headers(
-                context, request, requested_headers)
+                context, request, requested_headers
+            )
             response.headers.add(
-                'Access-Control-Allow-Headers', ','.join(allowed_headers))
+                "Access-Control-Allow-Headers", ",".join(allowed_headers)
+            )
 
         # Access-Control-Allow-Origin
-        requested_origin = request.headers.get('Origin')
+        requested_origin = request.headers.get("Origin")
 
         if requested_origin or preflight:
             allowed_origin = app.get_cors_allowed_origin(
-                context, request, requested_origin)
-            response.headers.add('Access-Control-Allow-Origin', allowed_origin)
-            if allowed_origin and allowed_origin != '*':
-                response.headers.add('Vary', 'Origin')
+                context, request, requested_origin
+            )
+            response.headers.add("Access-Control-Allow-Origin", allowed_origin)
+            if allowed_origin and allowed_origin != "*":
+                response.headers.add("Vary", "Origin")
 
         # Access-Control-Expose-Headers
         exposed_headers = app.get_cors_expose_headers(context, request)
-        response.headers.add('Access-Control-Expose-Headers',
-                             ','.join(exposed_headers))
+        response.headers.add(
+            "Access-Control-Expose-Headers", ",".join(exposed_headers)
+        )
 
         # Access-Control-Allow-Credentials
         allow_credentials = app.get_cors_allow_credentials(context, request)
         if allow_credentials is True:
-            response.headers.add('Access-Control-Allow-Credentials', 'true')
+            response.headers.add("Access-Control-Allow-Credentials", "true")
 
         if preflight:
             max_age = app.get_cors_max_age(context, request)
-            response.headers.add('Access-Control-Max-Age',
-                                 str(max_age))
+            response.headers.add("Access-Control-Max-Age", str(max_age))
 
         return response
+
     return cors_handler
