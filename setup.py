f53631653f98aad83d7f318cb6d2aa98d16237fb
Henri Hulski <henri.hulski@gazeta.pl>
2020-04-25 18:27:11 +0200

    Remove support for Python 2 and upgrade Python 3

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-import io
 from setuptools import setup, find_packages
 
 long_description = (
-    io.open('README.rst', encoding='utf-8').read()
-    + '\n\n' +
-    io.open('CHANGES.rst', encoding='utf-8').read())
+    open('README.rst', encoding='utf-8').read() + '\n\n'
+    + open('CHANGES.rst', encoding='utf-8').read())
 
 version = '0.3.dev0'
 
@@ -15,10 +13,12 @@ setup(name='more.cors',
       classifiers=[
           'Intended Audience :: Developers',
           'License :: OSI Approved :: BSD License',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: Implementation :: PyPy',
       ],
       keywords='morepath CORS',
       author='Izhar Firdaus',
@@ -31,7 +31,7 @@ setup(name='more.cors',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          'morepath',
+          'morepath >= 0.19',
       ],
       entry_points={
           'morepath': [
